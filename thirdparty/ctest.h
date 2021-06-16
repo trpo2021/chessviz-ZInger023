5e04b972a079d356031e7f421e65df0ce6c62a42
Mikhail Kulinich <ZInger023@users.noreply.github.com>
2021-06-16 12:02:23 +0300

    Шалость удалась?

--- thirdparty/ctest.h
+++ thirdparty/ctest.h
@@ -1,4 +1,4 @@
-/* Copyright 2011-2021 Bas van den Berg
+/* Copyright 2011-2021 Bas van den Berg1
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
